e4bf3b1291474360835613738a342f0881257406
ssp <swalehapeerzade@gmail.com>
2023-01-12 23:12:25 +0000

    commit

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,22 +2,47 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+
 public class DataSource {
 
   public static Connection connect() {
-    String db_file =   "jdbc:sqlite:resources/bank.db";
+    String db_file = "jdbc:sqlite:resources/bank.db";
     Connection connection = null;
-    try{
+    try {
       connection = DriverManager.getConnection(db_file);
       System.out.println("we're connected");
-    } catch(SQLException e){
+    } catch (SQLException e) {
       e.printStackTrace();
     }
     return connection;
   }
 
-  public static void main(String args[]){
-    connect();
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setString(1, username);
+        try(ResultSet resultSet = statement.executeQuery()){
+          customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+        }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
+  public static void main(String args[]) {
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
